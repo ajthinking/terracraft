c7f85303c7b7e5268640c9fda274eaae7b50e987
Anders Jurisoo <jurisoo@hotmail.com>
2019-02-27 17:36:55 +0800

    Added Tile class and User fix

--- database/migrations/2019_02_27_093443_create_tiles_table.php
+++ database/migrations/2019_02_27_093443_create_tiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tiles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nickname');
-            $table->string('avatar');
-            $table->string('provider');
-            $table->string('provider_id');
-            $table->rememberToken();
-            $table->timestamps();            
+            $table->timestamps();
         });
     }
 
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tiles');
     }
 }
