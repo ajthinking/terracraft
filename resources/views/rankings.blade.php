ce0f1ea6dc6a345f44ee3031a8a210e07d10298a
Anders Jurisoo <jurisoo@hotmail.com>
2019-03-11 16:55:57 +0800

    Somewthat more beautiful rankings page

--- resources/views/rankings.blade.php
+++ resources/views/rankings.blade.php
@@ -3,10 +3,12 @@
   <div class="flex items-center flex-col">
   <h3 class="mb-8 mt-4">Number of acres conquered</h3>
       @foreach($user_rankings as $user_ranking)
-      <div class="flex items-center">
-        <img class="w-10 h-10 rounded-full mr-4" src="{{ $user_ranking->avatar }}" alt="Avatar of Jonathan Reinink">
-        <div class="text-sm">
-          <p class="text-black leading-none">{{ $user_ranking->nickname }}</p>
+      <div class="flex items-center justify-between w-full max-w-sm">
+        <div class="flex items-center">
+          <img class="w-10 h-10 rounded-full mr-4" src="{{ $user_ranking->avatar }}" alt="Avatar of Jonathan Reinink">
+          <div class="text-sm">
+            <p class="text-black leading-none">{{ $user_ranking->nickname }}</p>
+          </div>
         </div>
         <p class="ml-8 text-black text-4xl">{{ $user_ranking->number_of_tiles }}</p>
       </div>
